521cfeba85e9936aa9fedbff5f351a6bc0f2fce0
steinnp <steiniell@gmail.com>
2015-10-13 17:40:49 +0000

    refactoring code for delimiter \n

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -7,29 +7,23 @@ public class Calculator {
     public static int add(String text){
         if(text == "")
             return 0;
-        else if(text.contains(",") || text.contains("\n")){
-            List<String> tempNumbers = parseNumbers(text);
-            List<String> numbers = parseNewLine(tempNumbers);
+        else{ 
+            List<String> convert = new ArrayList<String>();
+            convert.add(text);
+            List<String> tempNumbers = parseDelim(convert, ",");
+            List<String> numbers = parseDelim(tempNumbers, "\n");
             return sum(numbers);
         }
-        else
-            return toInt(text);
-        
-
     }
 
     public static int toInt(String number){
         return Integer.parseInt(number);
     }
 
-    public static List<String> parseNumbers(String numbers){
-        return Arrays.asList(numbers.split(","));
-    }
-
-    public static List<String> parseNewLine(List<String> text){
+    public static List<String> parseDelim(List<String> text, String delim){
         List<String> numbers = new ArrayList<String>();
         for(int i = 0; i < text.size(); i++){
-            String[] tempNumbers = text.get(i).split("\n");
+            String[] tempNumbers = text.get(i).split(delim);
             for(int j = 0; j < tempNumbers.length; j++){
                 numbers.add(tempNumbers[j]);
             }
