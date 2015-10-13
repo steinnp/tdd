00acb4552b3395f7b2879e630245b5d8104dde69
steinnp <steiniell@gmail.com>
2015-10-13 16:30:42 +0000

    tests for two numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -13,6 +13,7 @@ public class CalculatorTest {
         public void testEmptyString() {
             assertEquals(0, Calculator.add(""));
         }
+
     @Test
         public void testOneNumber(){
             assertEquals(1, Calculator.add("1"));
@@ -20,5 +21,14 @@ public class CalculatorTest {
             assertEquals(3, Calculator.add("3"));
             assertEquals(4, Calculator.add("4"));
             assertEquals(5, Calculator.add("5"));
+        }
+
+    @Test
+        public void testTwoNumbers(){
+            assertEquals(2, Calculator.add("1,1"));
+            assertEquals(4, Calculator.add("2,2"));
+            assertEquals(6, Calculator.add("3,3"));
+            assertEquals(8, Calculator.add("4,4"));
+            assertEquals(10, Calculator.add("5,5"));
         }
 }
