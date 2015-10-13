12964c2417c09fc34796134bc2c85eec8db776cd
steinnp <steiniell@gmail.com>
2015-10-13 16:22:59 +0000

    Tests for one number

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -13,4 +13,12 @@ public class CalculatorTest {
         public void testEmptyString() {
             assertEquals(0, Calculator.add(""));
         }
+    @Test
+        public void testOneNumber(){
+            assertEquals(1, Calculator.add("1"));
+            assertEquals(2, Calculator.add("2"));
+            assertEquals(3, Calculator.add("3"));
+            assertEquals(4, Calculator.add("4"));
+            assertEquals(5, Calculator.add("5"));
+        }
 }
