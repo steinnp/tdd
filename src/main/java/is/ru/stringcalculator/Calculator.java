09ac163ea6698ce3fc42463251b150c9657d92ac
steinnp <steiniell@gmail.com>
2015-10-13 17:19:47 +0000

    working code for delimiter \n

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,13 +1,16 @@
 package is.ru.stringcalculator;
 
+import java.util.*;
+
 public class Calculator {
 
     public static int add(String text){
         if(text == "")
             return 0;
-        else if(text.contains(",")){
-            String[] numbers = parseNumbers(text);
-            return sum(parseNumbers(text));
+        else if(text.contains(",") || text.contains("\n")){
+            List<String> tempNumbers = parseNumbers(text);
+            List<String> numbers = parseNewLine(tempNumbers);
+            return sum(numbers);
         }
         else
             return toInt(text);
@@ -19,14 +22,25 @@ public class Calculator {
         return Integer.parseInt(number);
     }
 
-    public static String[] parseNumbers(String numbers){
-        return numbers.split(",");
+    public static List<String> parseNumbers(String numbers){
+        return Arrays.asList(numbers.split(","));
     }
 
-    public static int sum(String[] numbers){
+    public static List<String> parseNewLine(List<String> text){
+        List<String> numbers = new ArrayList<String>();
+        for(int i = 0; i < text.size(); i++){
+            String[] tempNumbers = text.get(i).split("\n");
+            for(int j = 0; j < tempNumbers.length; j++){
+                numbers.add(tempNumbers[j]);
+            }
+        }
+       return numbers; 
+    }
+
+    public static int sum(List<String> numbers){
         int sum = 0;
-        for(int i = 0; i < numbers.length; i++){
-            sum += toInt(numbers[i]);
+        for(int i = 0; i < numbers.size(); i++){
+            sum += toInt(numbers.get(i));
         }
         return sum;
     }
