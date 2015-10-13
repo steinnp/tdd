632b696885aaabc1b56dd45add0a59c53f3cd8ad
steinnp <steiniell@gmail.com>
2015-10-13 16:28:29 +0000

    add function working for one number

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,6 +3,10 @@ package is.ru.stringcalculator;
 public class Calculator {
 
     public static int add(String text){
+        if(text == "")
             return 0;
+        else
+            return Integer.parseInt(text);
+
     }
 }
