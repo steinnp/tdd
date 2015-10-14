6e330275c61244f3442ddb49086584d580630143
steinnp <steiniell@gmail.com>
2015-10-14 23:06:09 +0000

    refactoring and documentation (oops)

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -10,43 +10,47 @@ public class Calculator {
         else{ 
             List<String> convert = new ArrayList<String>();
             convert.add(text);
-            List<String> tempNumbers = parseDelim(convert, "\n");
-            String delimCheck = tempNumbers.get(0);
-                if(delimCheck.matches("//(.*)") == true){
-                    String delim = delimCheck.substring(2,3);
-                    tempNumbers.remove(0);
-                    tempNumbers.add(delim);
-                    tempNumbers = parseDelim(tempNumbers, delim);
-                }
-            List<String> numbers = parseDelim(tempNumbers, ",");
+            convert = parseDelim(convert, "\n");
+            String delimCheck = convert.get(0);
+            //if a new delimiter is declared we get the delimiter and
+            //parse the strings into substrings.
+            if(delimCheck.matches("//(.*)") == true){
+                String newDelim = getDelim(delimCheck);
+                convert.remove(0);
+                convert = parseDelim(convert, newDelim);
+            }
+            List<String> numbers = parseDelim(convert, ",");
             return sum(numbers);
         }
     }
-
+    // turns a number equivalent of string to int
     public static int toInt(String number){
         return Integer.parseInt(number);
     }
+    //parses any delimiter declared at the start of a string with the correct
+    //syntax
+    public static String getDelim(String delimCheck){
+        String delim = delimCheck.substring(2,3);
+        return delim;
+    }
 
+    //breaks down a list of strings into substrings with delim as delimiter
     public static List<String> parseDelim(List<String> text, String delim){
         List<String> numbers = new ArrayList<String>();
         for(int i = 0; i < text.size(); i++){
-            String[] tempNumbers = text.get(i).split(delim);
-            for(int j = 0; j < tempNumbers.length; j++){
-                numbers.add(tempNumbers[j]);
+            String[] convert = text.get(i).split(delim);
+            for(int j = 0; j < convert.length; j++){
+                numbers.add(convert[j]);
             }
         }
        return numbers; 
     }
-
+    //computes the sum of the string equivalent of integers.
     public static int sum(List<String> numbers){
         int sum = 0;
         for(int i = 0; i < numbers.size(); i++){
             sum += toInt(numbers.get(i));
         }
         return sum;
-    }
-
-    public static void main(String[] args){
-       int temp = add("\\\\;\n2,3;3\n5"); 
     }
 }
